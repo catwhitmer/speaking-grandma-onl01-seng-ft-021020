71fef2cc3d9313f0ce69c0dc98dbb478ee336a7b
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-11 00:01:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
  
-elsif phrase == "I LOVE YOU GRANDMA!"
+elsif phrase = "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 else 
     return "HUH?! SPEAK UP, SONNY!"
